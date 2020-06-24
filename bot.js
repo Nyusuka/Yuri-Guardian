77e3b2284fbd79fac17c2f7ff04cb30ea02431ee
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2020-06-24 17:43:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,39 +8,3 @@ bot.login(token);
 bot.on("ready", async message =>{
     bot.user.setActivity("chat perché ! 😹")
 });
-
-//Boucle contenant la blacklist :
-bot.on("guildMemberAdd", member =>{
-    if(member.id = "376500913465982976"){
-        const reason = "Cette personne a été blacklisté car considéré(e) comme potentiellement nuisible au bien-être du serveur."
-        member.ban(reason);
-        console.log(`${member.username}#${member.discriminator} a été banni avec succès.`)
-    }
-});
-
-//Boucle contenant les commandes :
-bot.on("message", async message => {
-
-    //Constantes pour l'ID de la propriétaire et des contributeurs du bot. :
-        const ownerID = "382500192907165717"; //Nyusuka ; propriétaire du bot.
-        const contributorID_01 = "452410888910143499"; //Alyssia ; propriétaire du serveur Fans de yuri FR.
-
-    //Autres constantes pour le bon focntionnement des commandes. :
-        const prefix = '<!';
-            if(message.content.indexOf(prefix) !== 0) return;
-        const args = message.content.slice(prefix.length).trim().split(/ +/g); 
-        const command = args.shift().toLowerCase();
-
-//Commandes réservées à Nyusuka et aux contributeurs du développement du bot :
-
-    //Commande pour changer le statut du bot. [&aktivität] :
-        if(command === "aktivität"){
-            const aktivität = args.join(" ");
-            if(message.author.id !== ownerID && message.author.id !== contributorID_01) return message.channel.send("Désolée, mais pour des raisons pratiques, cette commande n'est utilisable que par Nyusuka ou Alyssia.");
-            if(!aktivität) return message.reply("AKTIVITÄT ! SCHNELL !");
-            bot.user.setActivity(aktivität);
-            message.delete().catch(O_o=>{});
-            console.log(`Le statut du bot à été changé pour "Joue à ${aktivität}" par ${message.author.username}#${message.author.discriminator} (ID : ${message.author.id}).`);
-        };
-
-});
